be45d5803dccc4d5db2cfbc99ca953c15ecca574
ByNeo <kaanhas.52@gmail.com>
2017-06-20 23:00:20 +0300

    TestMethod1 was added.

---
+++ src/Nuget.NetCore.TestPackage/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nuget.NetCore.TestPackage
+{
+    public class TestClass
+    {
+        Logger logger = LogManager.GetCurrentClassLogger();
+
+        public TestClass()
+        {
+        }
+
+        public string TestMethod1()
+        {
+            return "TestMethod1";
+        }
+    }
+}
