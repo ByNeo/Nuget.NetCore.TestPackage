10f349c98963bd91f779da806dcd0bc99ce8c708
Kaan HAS <kaanhas.52@gmail.com>
2017-06-27 13:06:39 +0300

    Update build.ps1

--- .build/build.ps1
+++ .build/build.ps1
@@ -3,7 +3,7 @@
 dotnet --version
 
 dotnet restore
-dotnet pack --configuration release   -o artifacts
+dotnet pack .\src\Nuget.NetCore.TestPackage\ --configuration release   -o artifacts
 
 # remove this when we get clean builds
 Get-ChildItem artifacts\*.nupkg | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
